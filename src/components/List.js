02b3aca0df40e82a92b352c70dddc311df7f95c4
Oli Rooker <oliver.rooker@gmail.com>
2020-12-06 13:56:53 +0000

    change css class when user completes a task

--- src/components/List.js
+++ src/components/List.js
@@ -5,8 +5,8 @@ const List = ({ todos, deleteToDo, toggleIsCompleted }) => {
     return (
         <ul className="list-container">
             {todos.map((todo) => {
-                return <li className="list-item" key={todo.item} id={todo.item} onClick={() => { toggleIsCompleted(todo.item) }}>
-                    <i class="fa fa-square-o"></i>
+                return <li className={`${todos.isCompleted ? "completed-list-item" : "list-item"}`} key={todo.item} id={todo.item} onClick={() => { toggleIsCompleted(todo.item) }}>
+                    <i className={`${todos.isCompleted ? "fa fa-check-square" : "fa fa-square-o"}`}></i>
                     {todo.item}
                     <button className="delete-button" onClick={() => { deleteToDo(todo.item) }}><i className="fa fa-trash"></i></button>
                 </li>;
