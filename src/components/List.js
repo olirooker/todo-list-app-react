c707c9b9a04882058b36faac49c9e5df36822d6b
Oli Rooker <oliver.rooker@gmail.com>
2020-12-07 11:20:31 +0000

    onClick of tick box to run the toggleIsComplete

--- src/components/List.js
+++ src/components/List.js
@@ -6,7 +6,7 @@ const List = ({ todos, deleteToDo, toggleIsCompleted }) => {
         <ul className="list-container">
             {todos.map((todo) => {
                 return <li key={todo.item} id={todo.item} className="list-element-container">
-                    <i className={todo.isCompleted ? "fa fa-check-square" : "fa fa-square-o"}></i>
+                    <i className={todo.isCompleted ? "fa fa-check-square" : "fa fa-square-o"} onClick={() => { toggleIsCompleted(todo.item) }}></i>
                     <p className={todo.isCompleted ? "completed-list-item" : "list-item"} onClick={() => { toggleIsCompleted(todo.item) }}>{todo.item}</p>
                     <button className="delete-button" onClick={() => { deleteToDo(todo.item) }}><i className="fa fa-trash"></i></button>
                 </li>;
