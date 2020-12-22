e7fe41eb5503a4be7f473bb973ed23e6fca30006
Oli Rooker <oliver.rooker@gmail.com>
2020-12-22 14:02:25 +0000

    clean up code in the Filter component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,16 +1,29 @@
-import React from 'react';
-
 const Filter = ({ changeFilter }) => {
-
-
-
-    return (
-        <div className="filter-buttons">
-            <button onClick={() => { changeFilter('all') }}>All</button>
-            <button onClick={() => { changeFilter('active') }} >Active</button>
-            <button onClick={() => { changeFilter('completed') }} >Complete</button>
-        </div>
-    )
+  return (
+    <div className="filter-buttons">
+      <button
+        onClick={() => {
+          changeFilter("all");
+        }}
+      >
+        All
+      </button>
+      <button
+        onClick={() => {
+          changeFilter("active");
+        }}
+      >
+        Active
+      </button>
+      <button
+        onClick={() => {
+          changeFilter("completed");
+        }}
+      >
+        Complete
+      </button>
+    </div>
+  );
 };
 
 export default Filter;
